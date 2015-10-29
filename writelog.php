61a667fc73d9fbcbaaeadb7926f7ae6409d3975f
edisonthk-mac <edisonthk@gmail.com>
2015-10-29 18:39:42 +0900

    optimize write log

--- writelog.php
+++ writelog.php
@@ -11,12 +11,12 @@ if(!empty($action)) {
     $parser = Parser::create();
     $result = $parser->parse($_SERVER['HTTP_USER_AGENT']);
 
-    $data = date('Y-m-d H:i:s')."\t".specificLengthWords($result->os->family ,6)."\t".specificLengthWords($result->ua->family ,6)."\t".$action."\n";
+    $data = date('Y-m-d H:i:s')."\t".specificLengthWords($result->toString() ,30)."\t".$action."\n";
 
     file_put_contents("./action.log", $data ,FILE_APPEND);    
 }
 
 function specificLengthWords($word, $len)
 {
-    return substr($word."              ", 0,$len);
+    return substr($word."                                          ", 0,$len);
 }
